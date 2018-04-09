d121c44c1cc5b3bd2bcff09ae00fbcff2924a12c
Michael <36387692+Mikleodeon@users.noreply.github.com>
2018-04-09 16:22:09 -0400

    Initial Commit

---
+++ Source/TestinGrounds/TestinGroundsGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "TestinGroundsGameMode.h"
+#include "TestinGroundsHUD.h"
+#include "TestinGroundsCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATestinGroundsGameMode::ATestinGroundsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ATestinGroundsHUD::StaticClass();
+}
