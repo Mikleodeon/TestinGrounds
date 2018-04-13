3c32532f8513107b545216c43a4401720d7ef230
Unknown <greggwilliam15@gmail.com>
2018-04-12 23:19:01 -0400

    Hearing

--- Source/TestinGrounds/TestinGroundsCharacter.cpp
+++ Source/TestinGrounds/TestinGroundsCharacter.cpp
@@ -164,6 +164,8 @@ void ATestinGroundsCharacter::OnFire()
 
 				// spawn the projectile at the muzzle
 				World->SpawnActor<ATestinGroundsProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+
+				MakeNoise(1.f, this, GetActorLocation(), 0.f, FName("Fire"));
 			}
 		}
 	}
